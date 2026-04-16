6130e6639e0945f2380dcbfc54be325fc01749df
R-PG22 <tsukisigure1225@gmail.com>
2026-04-16 15:21:30 +0900

    クリエイティブの追加

--- src/main/java/r_pg/apples/ModItems.java
+++ src/main/java/r_pg/apples/ModItems.java
@@ -1,17 +1,31 @@
 package r_pg.apples;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 public class ModItems {
+	public static final ResourceKey<CreativeModeTab> CUSTOM_ITEM_GROUP_KEY = ResourceKey.create(BuiltInRegistries.CREATIVE_MODE_TAB.key(), new ResourceLocation(Apples.MOD_ID, "item_group"));
+	public static final CreativeModeTab CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
+			.icon(() -> new ItemStack(ModItems.SLICED_APPLE))
+			.title(Component.translatable("itemGroup.apples"))
+			.build();
+
 	public static void initialize(){
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.INGREDIENTS)
-				.register((itemGroup) -> itemGroup.accept(ModItems.SLICED_APPLE));
+		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, CUSTOM_ITEM_GROUP_KEY, CUSTOM_ITEM_GROUP);
+
+		ItemGroupEvents.modifyEntriesEvent(CUSTOM_ITEM_GROUP_KEY).register(itemGroup -> {
+			itemGroup.accept(ModItems.SLICED_APPLE);
+		});
 	}
 
 	public static Item register(Item item, String id) {
