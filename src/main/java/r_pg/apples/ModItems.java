622165a6a7b2bd99e456627e2053bde95c82eebb
Kyonshi <155651185+kyonshi0104@users.noreply.github.com>
2026-04-16 15:48:51 +0900

    未使用のimportを削除

--- src/main/java/r_pg/apples/ModItems.java
+++ src/main/java/r_pg/apples/ModItems.java
@@ -9,7 +9,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
@@ -33,11 +32,7 @@ public class ModItems {
 		// Create the identifier for the item.
 		ResourceLocation itemID = new ResourceLocation(Apples.MOD_ID, id);
 
-		// Register the item.
-		Item registeredItem = Registry.register(BuiltInRegistries.ITEM, itemID, item);
-
-		// Return the registered item!
-		return registeredItem;
+		return Registry.register(BuiltInRegistries.ITEM, itemID, item);
 	}
 
 	public static final Item SLICED_APPLE = register(
